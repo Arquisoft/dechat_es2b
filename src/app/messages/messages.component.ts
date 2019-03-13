893673b1826a010da785f3ef9a0f95168230e701
Andres <UO244499@uniovi.es>
2019-03-13 22:45:02 +0100

    Continuing with messages

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,16 +2,22 @@ import {Component, Input, OnInit} from '@angular/core';
 import {RdfService} from '../services/rdf.service';
 import {Router} from '@angular/router';
 
+declare let $rdf: any;
+const SIOC = $rdf.Namespace('http://rdfs.org/sioc/spec/');
+const SCHEMA = $rdf.Namespace('http://schema.org/Message');
+const CREATIVEWORK = $rdf.Namespace('http://schema.org/CreativeWork');
+
 @Component({
   selector: 'app-mensajes',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
+
 export class MessagesComponent implements OnInit {
 
   @Input('content') content: string;
   date: Date;
-  receiver;
+  recipient;
 
   constructor() { }
 
@@ -20,8 +26,20 @@ export class MessagesComponent implements OnInit {
     this.date = new Date();
   }
 
-  addReceiver(urlReceiver) {
-    this.receiver = urlReceiver;
+  addRecipient(urlRecipient) {
+    this.recipient = urlRecipient;
+  }
+
+  getContentInArraySchema() {
+    const array = [];
+    array.push(SCHEMA('recipient'));
+    array.push(this.recipient);
+    array.push(SCHEMA('dateSent'));
+    array.push(this.date.toISOString());
+    array.push(SCHEMA('messageAttachment'));
+    array.push(CREATIVEWORK('text'));
+    array.push(this.content);
+    return array;
   }
 
   ngOnInit() {
