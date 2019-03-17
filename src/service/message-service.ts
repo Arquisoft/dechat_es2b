b803ae1368de0d3e1a8ccd9eff0f472cebae8bd9
enolgargon <enolgargon@gmail.com>
2019-03-17 23:42:15 +0100

    Clase servicio de mensajes
    Declarados los métodos en la clase de lógica de negocio de los mensajes
    
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- src/service/message-service.ts
+++ src/service/message-service.ts
@@ -1,2 +1,13 @@
+import {Message} from '../model/message';
+import {Contact} from '../model/contact';
+
 export class MessageService {
+  sendMessage = async (message: Message) => {
+    // TODO
+  };
+
+  getMessages = (contact: Contact): Message[] => {
+    // TODO
+    return [];
+  };
 }
