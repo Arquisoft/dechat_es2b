1ccec4ed502de00881cbbfc83c568a5d2a732d5a
enolgargon <enolgargon@gmail.com>
2019-03-19 21:45:24 +0100

    Completada la vista de contactos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactComponent } from './contact/contact.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    ContactsComponent,
+    ContactComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
