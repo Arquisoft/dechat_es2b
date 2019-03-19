58e10c4cd6d08b9e900205a245583d3efb27dd9b
enolgargon <enolgargon@gmail.com>
2019-03-19 23:09:58 +0100

    Comportamiento del componente mensajes

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../../model/contact';
+import {Message} from '../../model/message';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent {
+  contact: Contact;
+  messages: Message[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.messages = [];
   }
 
+  showMessages = async () => {
+
+  }
+
+  selectConversation(contact: Contact) {
+    this.contact = contact;
+    this.showMessages();
+  }
 }
