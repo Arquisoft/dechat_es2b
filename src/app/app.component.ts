cd4b588b3a901fcc3f19533db873df8ef084dfe2
Andres <UO244499@uniovi.es>
2019-03-20 19:05:59 +0100

    Serializar y deserializar mensajes a json terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Serializer} from '../repository/util/serializer';
-import {Message} from '../model/message';
-import {Contact} from '../model/contact';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +7,4 @@ import {Contact} from '../model/contact';
 })
 export class AppComponent {
   title = 'dechat';
-
-  onClick() {
-    // const contactFrom = new Contact('https://acg96.inrupt.net/profile/me#', 'acg96');
-    // const contactTo = new Contact('https://enolgargon.inrupt.net/profile/me#', 'enol');
-    // const mess1 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 1');
-    // const mess2 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 2');
-    // const mess3 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 3');
-    // const mess4 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 4');
-    // const mess = [];
-    // mess.push(mess1);
-    // mess.push(mess2);
-    // mess.push(mess3);
-    // mess.push(mess4);
-    // const json = Serializer.serializeMessages(mess);
-    // const mensajes = Serializer.deserializeMessages(json);
-    // alert(mensajes.length);
-    // for (let i = 0; i < mensajes.length; ++i) {
-    //   alert(mensajes[i].from.nickname + ' - ' + mensajes[i].from.urlPod + ' - ' + mensajes[i].to.nickname + ' - ' + mensajes[i].to.urlPod + ' - ' + new Date(mensajes[i].date) + ' - ' + mensajes[i].text);
-    // }
-  }
-
-
 }
--- src/repository/util/serializer.ts
+++ src/repository/util/serializer.ts
@@ -1,22 +1,15 @@
-import * as N3 from 'N3';
 import {Message} from '../../model/message';
 import {Contact} from '../../model/contact';
-
-// const { namedNode, literal, defaultGraph, quad } = N3.DataFactory;
 
 export class Serializer {
   // TODO Esquemas que hay que utilizar:
   //        https://schema.org/Message
   //        http://xmlns.com/foaf/spec/#term_knows
   //        http://xmlns.com/foaf/spec/#term_nick
+
   static serializeMessages = (messages: Message[]): string => {
     const json = JSON.stringify(messages);
     return json;
-
-    // 1. Utilizando la libreria, crear un grafo vacio
-    // 2. Añadir los mensajes al grafo
-    // 3. Obtener la cadena de texto que representa el grafo. Lo hace la librería
-    // return '';
   }
 
   static deserializeMessages = (data: string): Message[] => {
@@ -29,13 +22,6 @@ export class Serializer {
       messages.push(messg);
     }
     return messages;
-
-    // 1. Utilizando la librería, parsear el atributo data. Es decir, crear un grafo con los datos que contiene
-    // 2. Investiga un poco, creo que cada nodo puede devolverse como un mensaje
-    // 2.1 Si ya te da una estructura como los mensajes (Ojo estructura, aunque no se llame igual,
-    //     si tiene los mismo campos vale), return de eso
-    // 2.2 Si no, recorrer la respuesta de la libreria y crear objetos de tipo mensaje que se devuelven
-    // return [];
   }
 
   static serializeContact = (newContact: Contact, oldData: string): string => {
