ef1eda13a6349edd7262c8c5a31500fb58ad260a
Andres <UO244499@uniovi.es>
2019-03-20 19:03:05 +0100

    con mensajes de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Serializer} from '../repository/util/serializer';
+import {Message} from '../model/message';
+import {Contact} from '../model/contact';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dechat';
+
+  onClick() {
+    // const contactFrom = new Contact('https://acg96.inrupt.net/profile/me#', 'acg96');
+    // const contactTo = new Contact('https://enolgargon.inrupt.net/profile/me#', 'enol');
+    // const mess1 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 1');
+    // const mess2 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 2');
+    // const mess3 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 3');
+    // const mess4 = new Message(contactFrom, contactTo, new Date(), 'Prubea mensaje 4');
+    // const mess = [];
+    // mess.push(mess1);
+    // mess.push(mess2);
+    // mess.push(mess3);
+    // mess.push(mess4);
+    // const json = Serializer.serializeMessages(mess);
+    // const mensajes = Serializer.deserializeMessages(json);
+    // alert(mensajes.length);
+    // for (let i = 0; i < mensajes.length; ++i) {
+    //   alert(mensajes[i].from.nickname + ' - ' + mensajes[i].from.urlPod + ' - ' + mensajes[i].to.nickname + ' - ' + mensajes[i].to.urlPod + ' - ' + new Date(mensajes[i].date) + ' - ' + mensajes[i].text);
+    // }
+  }
+
+
 }
--- src/repository/util/serializer.ts
+++ src/repository/util/serializer.ts
@@ -1,5 +1,8 @@
+import * as N3 from 'N3';
 import {Message} from '../../model/message';
 import {Contact} from '../../model/contact';
+
+// const { namedNode, literal, defaultGraph, quad } = N3.DataFactory;
 
 export class Serializer {
   // TODO Esquemas que hay que utilizar:
@@ -7,35 +10,53 @@ export class Serializer {
   //        http://xmlns.com/foaf/spec/#term_knows
   //        http://xmlns.com/foaf/spec/#term_nick
   static serializeMessages = (messages: Message[]): string => {
-    // TODO
+    const json = JSON.stringify(messages);
+    return json;
+
     // 1. Utilizando la libreria, crear un grafo vacio
-
     // 2. Añadir los mensajes al grafo
-
     // 3. Obtener la cadena de texto que representa el grafo. Lo hace la librería
-    return '';
-  };
+    // return '';
+  }
 
   static deserializeMessages = (data: string): Message[] => {
-    // TODO
-    // 1. Utilizando la librería, parsear el atributo data. Es decir, crear un grafo con los datos que contiene
+    const messages = [];
+    const objJSON = JSON.parse(data);
+    for (let i = 0; i < objJSON.length; ++i) {
+      const from = new Contact(objJSON[i]['_from']['_urlPod'], objJSON[i]['_from']['_nickname']);
+      const to = new Contact(objJSON[i]['_to']['_urlPod'], objJSON[i]['_to']['_nickname']);
+      const messg = new Message(from, to, objJSON[i]['_date'], objJSON[i]['_text']);
+      messages.push(messg);
+    }
+    return messages;
 
+    // 1. Utilizando la librería, parsear el atributo data. Es decir, crear un grafo con los datos que contiene
     // 2. Investiga un poco, creo que cada nodo puede devolverse como un mensaje
     // 2.1 Si ya te da una estructura como los mensajes (Ojo estructura, aunque no se llame igual,
     //     si tiene los mismo campos vale), return de eso
     // 2.2 Si no, recorrer la respuesta de la libreria y crear objetos de tipo mensaje que se devuelven
-    return [];
-  };
+    // return [];
+  }
 
   static serializeContact = (newContact: Contact, oldData: string): string => {
     // TODO Hacer antes los mensajes
     // 1. Utilizando la libreria, parsear el atributo oldData. Es decir, crear un grafo con la información textual
-
+    // const store = N3.Store();
+    // const myQuad = quad(
+    //   namedNode('https://ruben.verborgh.org/profile/#me'),
+    //   namedNode('http://xmlns.com/foaf/0.1/givenName'),
+    //   literal('Ruben', 'en'),
+    //   defaultGraph(),
+    // );
+    // console.log(myQuad.subject.value);         // https://ruben.verborgh.org/profile/#me
+    // console.log(myQuad.object.value);          // Ruben
+    // console.log(myQuad.object.datatype.value); // http://www.w3.org/1999/02/22-rdf-syntax-ns#langString
+    // console.log(myQuad.object.language);       // en
     // 2. A ese grafo, añadirle un elemento nuevo, que es el que esta en el atributo newContact
 
     // 3. Devolver la representación en modo texto de ese grafo con formato rdf. Lo hace la librería
     return '';
-  };
+  }
 
   static deserializeContacts = (data: string): Contact[] => {
     // TODO Hacer antes los mensajes
@@ -46,5 +67,5 @@ export class Serializer {
     //    OJO: Obligatorio recorrer en este caso porque hay cosas que no son contactos
 
     return [];
-  };
+  }
 }
