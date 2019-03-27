ac118563ec0a562ce4a5958db69448d561e4b25d
enolgargon <enolgargon@gmail.com>
2019-03-27 12:33:42 +0100

    Prueba del coveralls
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -11,17 +11,17 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coveralls'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/dechat'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    reporters: ['coverage', 'coveralls'],
+    coverageReporter: {
+      type: 'lcov',
+      dir: '../coverage/'
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
