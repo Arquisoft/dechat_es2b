416e7764eaefffc909253256663e74041c074ce1
enolgargon <enolgargon@gmail.com>
2019-03-28 19:44:11 +0100

    Más pruebas para el componenete de mensajes
    Signed-off-by: enolgargon <enolgargon@gmail.com>

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -5,6 +5,7 @@ import {MockRepository} from '../../repository/impl/mock-repository';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {MessageComponent} from '../message/message.component';
+import {Contact} from '../../model/contact';
 
 describe('MessagesComponent', () => {
   let component: MessagesComponent;
@@ -45,6 +46,33 @@ describe('MessagesComponent', () => {
       expect(repo.logedIn).toBeTruthy();
       component.logout();
       expect(repo.logedIn).toBeFalsy();
+    });
+  });
+
+  it('should can select a conversation', () => {
+    const contact = new Contact('htpps://prueba.mock.up/profile/card#me', 'prueba');
+    component.selectConversation(contact);
+    expect(component.contact).toBe(contact);
+  });
+
+  it('should can send a message pressing the button', () => {
+    const text = 'Hi, this is a test';
+    component.message = text;
+    component.sendMessage(null).then(() => {
+      expect(repo.messages.length).toBe(1);
+      expect(repo.messages[0].text).toBe(text);
+    });
+  });
+
+  it('should can send a message pressing intro', () => {
+    const text = 'Hi, this is a test';
+    component.message = text;
+    component.sendMessage(new KeyboardEvent('keydown', {
+      key: 'Enter',
+      code: '13'
+    })).then(() => {
+      expect(repo.messages.length).toBe(1);
+      expect(repo.messages[0].text).toBe(text);
     });
   });
 });
