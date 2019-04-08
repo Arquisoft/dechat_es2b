17fc07c1d719f0764ac4ee484de4f81f591e4da1
Tomás <nekuyshiki@gmail.com>
2019-04-08 07:39:42 +0200

    test

--- src/model/contact.spec.ts
+++ src/model/contact.spec.ts
@@ -4,4 +4,20 @@ describe('Contact', () => {
   it('should create an instance', () => {
     expect(new Contact('https://usuario2.pod.prueba/', 'Prueba')).toBeTruthy();
   });
+
+  it('should change the urlPod', () => {
+    const c = new Contact('https://usuario2.pod.pruea/', 'Prueba');
+    const u = c.isUnknown;
+    expect(c).toBeTruthy();
+    expect(u).toBe(c.isUnknown);
+    c.urlPod = 'https://usuario2.pod.prueba';
+    expect(c.urlPod).toBe('https://usuario2.pod.prueba');
+  });
+
+  it('should change the image', () => {
+    const c = new Contact('https://usuario2.pod.pruea/', 'Prueba');
+    expect(c).toBeTruthy();
+    c.imageUrl = 'algo';
+    expect(c.imageUrl).toBe('algo');
+  });
 });
