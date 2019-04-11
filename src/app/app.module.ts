51bf1b9e1f0f7fb5c1136d11b14c39d1d0c9b876
Andres <UO244499@uniovi.es>
2019-04-11 20:20:37 +0200

    Finalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {MessagesComponent} from './messages/messages.component';
 import {ContactsComponent} from './contacts/contacts.component';
