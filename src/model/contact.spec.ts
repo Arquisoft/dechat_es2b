f92938da338e8556891b0dc104f354e486d1ced6
Tomás <nekuyshiki@gmail.com>
2019-04-21 14:40:18 +0200

    añadidos test a model\contact, model\message, repository\util\serializer, service\login

--- src/model/contact.spec.ts
+++ src/model/contact.spec.ts
@@ -4,4 +4,19 @@ describe('Contact', () => {
   it('should create an instance', () => {
     expect(new Contact('https://usuario2.pod.prueba/', 'Prueba')).toBeTruthy();
   });
+  it('should change the urlPod', () => {
+    const c = new Contact('https://usuario2.pod.prueba/', 'Prueba');
+    const u = c.isUnknown;
+    expect(c).toBeTruthy();
+    expect(u).toBe(c.isUnknown);
+    c.urlPod = 'https://usuario2.pod.prueba';
+    expect(c.urlPod).toBe('https://usuario2.pod.prueba');
+  });
+
+  it('should change the image', () => {
+    const c = new Contact('https://usuario2.pod.pruea/', 'Prueba');
+    expect(c).toBeTruthy();
+    c.imageUrl = 'algo';
+    expect(c.imageUrl).toBe('algo');
+  });
 });
--- src/model/message.spec.ts
+++ src/model/message.spec.ts
@@ -7,4 +7,34 @@ describe('Message', () => {
     const c2 = new Contact('https://usuario2.pod.prueba/', 'Prueba2');
     expect(new Message(c1, c2, new Date(), 'Hola')).toBeTruthy();
   });
+  it('should give back the data', () => {
+    const c1 = new Contact('https://usuario.pod.prueba/', 'Prueba1');
+    const c2 = new Contact('https://usuario2.pod.prueba/', 'Prueba2');
+    const d = new Date();
+    const m = new Message(c1, c2, d, 'Hola');
+    expect(m).toBeTruthy();
+    expect(m.from).toBe(c1);
+    expect(m.to).toBe(c2);
+    expect(m.date).toBe(d);
+    expect(m.text).toBe('Hola');
+  });
+
+  it('should change the data', () => {
+    const c1 = new Contact('https://usuario.pod.pruea/', 'Pruea1');
+    const c2 = new Contact('https://usuario2.pod.pruea/', 'Pruea2');
+    const d = new Date();
+    const m = new Message(c1, c2, d, 'Hola');
+    const c1a = new Contact('https://usuario.pod.prueba/', 'Prueba1');
+    const c2a = new Contact('https://usuario2.pod.prueba/', 'Prueba2');
+    const da = new Date();
+    m.from = c1a;
+    m.to = c2a;
+    m.text = 'Hola2';
+    m.date = da;
+    expect(m).toBeTruthy();
+    expect(m.from).toBe(c1a);
+    expect(m.to).toBe(c2a);
+    expect(m.date).toBe(da);
+    expect(m.text).toBe('Hola2');
+  });
 });
