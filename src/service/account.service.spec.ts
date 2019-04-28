9b52f11ae51bd8507b2b9e6aab293caa2410bc75
enolgargon <enolgargon@gmail.com>
2019-04-28 23:10:02 +0200

    Configurados los test de account

--- src/service/account.service.spec.ts
+++ src/service/account.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
+import {Mock} from 'protractor/built/driverProviders';
+import {MockRepository} from '../repository/impl/mock-repository';
 
 describe('AccountService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service;
+  let mock;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(AccountService);
+    mock = new MockRepository();
+    service.repository.config = mock;
+  });
 
   it('should be created', () => {
-    const service: AccountService = TestBed.get(AccountService);
     expect(service).toBeTruthy();
   });
 });
