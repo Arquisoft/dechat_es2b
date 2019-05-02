7913ca57dbd5649d4b600070f16bddd81ccbcef7
Andres Casillas Garcia <UO244499@uniovi.es>
2019-05-02 23:03:31 +0200

    Added controller

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  identityProvider;
+  username;
+  password;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      // Aqui lo que quieras hacer estará la información en los atributos
+
+
+    }, (reason) => {
+      // If the modal is closed using the cross button or clicking out of it
+    });
   }
 
 }
